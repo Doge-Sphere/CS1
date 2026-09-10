608716c0460843d204e3f7f374e2861b6ce785e6
Brian <brianmonix83008@hotmail.com>
2026-09-10 14:55:02 -0400

    made it do math

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 
-print('Hello World!')
-print('Welcome to cs1')
+number1=10
+number2=15
+
+equation1=number1+number2
+equation2=number1-number2
+equation3=number1*number2
+equation4=number1/number2
+
+print(str(equation1)+" "+str(equation2)+" "+str(equation3)+" "+str(equation4))
