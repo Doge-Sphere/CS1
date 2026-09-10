efcd4c4b3fed9edaf5ed916990992acec52f65ef
Brian <brianmonix83008@hotmail.com>
2026-09-10 14:57:45 -0400

    made it stop doing math

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
 
-number1=10
-number2=15
-
-equation1=number1+number2
-equation2=number1-number2
-equation3=number1*number2
-equation4=number1/number2
-
-print(str(equation1)+" "+str(equation2)+" "+str(equation3)+" "+str(equation4))
